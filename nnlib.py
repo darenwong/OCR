c9941e3e79d35aec503512caad323fef7232b391
darenwong <37960802+darenwong@users.noreply.github.com>
2018-03-31 18:49:43 +0100

    Update nnlib.py

--- nnlib.py
+++ nnlib.py
@@ -3,3 +3,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 np.random.seed(1) # set a seed so that the results are consistent
+
+
+
+def layer_sizes(X, Y):
+    """
+    Arguments:
+    X -- input dataset of shape (input size, number of examples)
+    Y -- labels of shape (output size, number of examples)
+    
+    Returns:
+    n_x -- the size of the input layer
+    n_y -- the size of the output layer
+    """
+    ### START CODE HERE ### (≈ 3 lines of code)
+    n_x = X.shape[0] # size of input layer
+    n_y = Y.shape[0] # size of output layer
+    ### END CODE HERE ###
+    return (n_x, n_h, n_y)
+  
+def initialize_parameters(n_x, n_h, n_y):
+    """
+    Argument:
+    n_x -- size of the input layer
+    n_h -- size of the hidden layer
+    n_y -- size of the output layer
+    
+    Returns:
+    params -- python dictionary containing your parameters:
+                    W1 -- weight matrix of shape (n_h, n_x)
+                    b1 -- bias vector of shape (n_h, 1)
+                    W2 -- weight matrix of shape (n_y, n_h)
+                    b2 -- bias vector of shape (n_y, 1)
+    """
+    
+    np.random.seed(2) # we set up a seed so that your output matches ours although the initialization is random.
+    
+    n = [n_x] + n_h + [n_y]
+    parameters = {}
+    for i in range(len(n)-1):
+    
+      parameters["W" + str(i+1)] = np.random.randn(n[i+1],n[i])/100
+      parameters["b" + str(i+1)] = np.zeros((n[i+1],1))
+    return parameters
+  
+  
